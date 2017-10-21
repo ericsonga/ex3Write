d1fe6f14f1f004fdb4d273e2da9b1f45ade80b92
ericsonga <ericson@cc.gatech.edu>
2017-10-21 10:25:07 -0400

    udpated pavement.py
    update

--- pavement.py
+++ pavement.py
@@ -1,26 +1,34 @@
 import paver
+import paver
 from paver.easy import *
 import paver.setuputils
 paver.setuputils.install_distutils_tasks()
+from socket import gethostname
 import os, sys
+from os import environ
 
 from sphinxcontrib import paverutils
 
 sys.path.append(os.getcwd())
 
 home_dir = os.getcwd()
-from socket import gethostname
+
 
 master_url = None
 if master_url is None:
-    if gethostname() == 'web407.webfaction.com':
+    if 'RSHOST' in os.environ:
+        master_url = environ['RSHOST']
+    elif gethostname() in  ['web608.webfaction.com', 'rsbuilder']:
         master_url = 'http://interactivepython.org'
+    elif gethostname() == 'runestone-deploy':
+        master_url = 'https://runestone.academy'
     else:
         master_url = 'http://127.0.0.1:8000'
         
 master_app = 'runestone'
 serving_dir = "./build/Ex3Write"
 dest = '../../static'
+project_name = "Ex3Write"
 
 options(
     sphinx = Bunch(docroot=".",),
@@ -37,11 +45,16 @@ options(
                        'loglevel': 10,
                        'course_url':master_url,
                        'use_services': 'true',
-                       'basecourse': 'Ex3Write',
-                       'python3': 'true'
+                       'python3': 'true',
+                       'dburl': 'postgresql://runestone@localhost/runestone',
+                       'basecourse': 'Ex3Write'
                         }
     )
 )
 
-from runestone import build  # build is called implicitly by the paver driver.
+# Check to see if we are building on our Jenkins build server, if so use the environment variables
+# to update the DB information for this build
+if 'DBHOST' in environ and  'DBPASS' in environ and 'DBUSER' in environ and 'DBNAME' in environ:
+    options.build.template_args['dburl'] = 'postgresql://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}'.format(**environ)
 
+from runestone import build  # build is called implicitly by the paver driver.
